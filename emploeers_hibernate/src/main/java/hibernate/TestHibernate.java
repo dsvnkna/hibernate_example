c5d766890174e149e58fc805f4aa170de6bf44bc
dsvnkna <dsavinkina@gmail.com>
2017-12-05 16:04:04 +0200

    First commit

---
+++ emploeers_hibernate/src/main/java/hibernate/TestHibernate.java
@@ -0,0 +1,35 @@
+package hibernate;
+
+import hibernate.project.EmployeeEntity;
+import org.hibernate.Session;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestHibernate {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        // Add new Employee object 
+        EmployeeEntity emp = new EmployeeEntity();
+        emp.setEmail("demo-user@mail.com");
+        emp.setFirstName("demo");
+        emp.setLastName("user");
+
+        session.save(emp);
+        //List result = new List();
+        List result = session.createQuery( "from Employee" ).list();
+        for (Iterator iter = name.iterator(); iter.hasNext(); ) {
+            firstName name = (firstName) iter.next();
+            System.out.println( name.getFirstName() );
+        }
+
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+    }
+} 
